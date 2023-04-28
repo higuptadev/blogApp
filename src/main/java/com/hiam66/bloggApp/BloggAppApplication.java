8d39f64a8dd24f2e4dfaff162f8717fd9a8ad792
higuptadev <higuptadev2002@gmail.com>
2023-04-28 19:04:34 +0530

    my blog app

---
+++ src/main/java/com/hiam66/bloggApp/BloggAppApplication.java
@@ -0,0 +1,13 @@
+package com.hiam66.bloggApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiam66/bloggApp/BloggAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiam66.bloggApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
